8a53ca821215c24ddbcbef756fefb190dc980104
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2016-03-20 11:52:08 +0100

    Add vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'alpr_ruby'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :typhoeus
+end
